e9b95ea9d0d7afd6a73bc146751f75932cb492b6
fernandod-tecsup <luis.diaz.tr@tecsup.edu.pe>
2021-04-09 18:52:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/boletinlahermosa/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.boletinlahermosa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 4000);
+    }
+}
